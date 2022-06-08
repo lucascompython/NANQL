a69ab46040598b6965bd99eb5d83a2d642dae868
Lucas de Linhares <lucasdelinhares@gmail.com>
2022-06-08 22:45:14 +0100

    Initial Commit

---
+++ Query Language/Program.fs
@@ -0,0 +1,48 @@
+﻿open Query
+open Books
+open System.Text.Json
+open System
+
+let books = Books.getAll()
+
+// PUT INSIDE THIS RECORD THE MEMBERS
+type Things = {
+    Title : string
+    Author : string
+    Category : string
+    PublishYear : int
+    Rating : float
+}
+
+let interpret lst result = 
+    match result with
+    | Result.Ok res ->
+        let queryResult = execute res lst
+        List.iter (fun i -> printfn "%O" i) queryResult
+    | Result.Error err -> printfn "%O" err
+
+[<EntryPoint>]
+let main (args) = 
+    
+    if args.Length < 1 then
+        printfn "No file was specified..."
+    
+    elif args.Length > 2 then
+        printfn "Too many arguments were given..."
+
+    elif Array.contains args[0] [| "-h" ; "--help" |] then
+        printfn "This program expects two arguments, the first one being the NANQL file and the other being the json file."
+    else
+        
+        printfn "Interpreting %A with %A" args[0] args[1]
+        
+        let reader = System.IO.File.ReadAllText(args[1])
+        let jsonInput = JsonSerializer.Deserialize<Things list>(reader)
+        
+        let NANQLInput = System.IO.File.ReadAllText(args[0])
+
+
+        
+        NANQLInput |> parse |> interpret jsonInput
+
+    0
---
+++ Query Language/README.md
@@ -0,0 +1,41 @@
+# NANQL (Not a Normal Query Language)
+
+
+## What is NANQL?
+
+NANQL is a simple query language for json, that I made to learn better about parsers and F#. <br />
+It's syntax is very declarative and easy to understand (just like SQL).
+
+## Code Preview
+
+```NANQL
+filterby Category = 'Fantasy'
+orderby Rating asc
+take 1
+```
+
+## How to use it
+
+```powershell
+git clone https://github.com/lucascompython/NANQL.git
+cd NANQL
+dotnet run -- query.nanql data.json
+```
+
+
+
+## CAUTION
+
+In this example, I'm using a Book's record with it's own member (look [here](Program.fs#L9)), if you want to use a different "object", for now you need to change the Record in the Program.fs file.
+
+
+## TODOs
+
+- [ ] Polish the output
+- [ ] Add some more complex queries, for example let's say you have this piece of json: `[{"x": 2, "y": 0}, {"x": 3, "y": 1}, {"x": 4, "y": 1}]`, I wan't to add something like `SUM(X) WHERE Y > 0     (would equate to 7)` and `LIST(X) WHERE Y > 0    (would equate to [3,4])`
+- [ ] Making the act of change the type of the object easier.
+
+
+# LICENSE
+
+This project is licensed under the GPL3 license.
